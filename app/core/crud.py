39eec422d2554565e0fbd35fbbd4f6cd05bbf7b5
santana <luissantanaa>
2024-04-20 18:02:33 +0100

    updates crud operations and models

--- app/core/crud.py
+++ app/core/crud.py
@@ -1,23 +1,35 @@
+from datetime import datetime
 from sqlalchemy.orm import Session
 
 from . import models, schemas
 from .utils.utils import hashPassword, convertBytes, convertTimestamp, isValidDataPoint
 
 
-def get_user_data_points(db: Session):
-    return db.query(models.DataPoint).where(models.DataPoint.valid).all()
+def get_user_data_points(db: Session, start: datetime, end: datetime):
+    return (
+        db.query(models.DataPoint)
+        .where(models.DataPoint.valid)
+        .where(models.DataPoint.time >= start)
+        .where(models.DataPoint.time <= end)
+        .all()
+    )
 
 
-def get_admin_data_points(db: Session):
-    return db.query(models.DataPoint).all()
+def get_admin_data_points(db: Session, start: datetime, end: datetime):
+    return (
+        db.query(models.DataPoint)
+        .where(models.DataPoint.time >= start)
+        .where(models.DataPoint.time <= end)
+        .all()
+    )
 
 
 def create_data_point(db: Session, data_point: schemas.DataPointRecieve):
     converted_value = convertBytes(data_point.value)
     converted_time = convertTimestamp(data_point.time)
-    valid = isValidDataPoint(data_point.tags, data_point.time)
+    tags, valid = isValidDataPoint(data_point.tags, data_point.time)
     db_point = models.DataPoint(
-        time=converted_time, value=converted_value, valid=valid[1], tags=valid[0]
+        time=converted_time, value=converted_value, valid=valid, tags=tags
     )
     db.add(db_point)
     db.commit()
--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import (
     Boolean,
     Column,
+    DateTime,
     Integer,
     String,
     TIMESTAMP,
@@ -26,7 +27,7 @@ class DataPoint(Base):
     __tablename__ = "data_points"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    time = Column(TIMESTAMP)
+    time = Column(DateTime)
     value = Column(DOUBLE_PRECISION)
     valid = Column(Boolean)
     tags = Column(ARRAY(String))
