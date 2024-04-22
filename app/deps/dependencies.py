794a36775c8b49f8de3c95f67625dc7ca95664a3
santana <luissantanaa>
2024-04-22 10:47:17 +0100

    moves dependencies to deps folder

---
+++ app/deps/dependencies.py
@@ -0,0 +1,55 @@
+import os
+from datetime import datetime
+from typing import Any, Union
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import ValidationError
+from jose import jwt
+
+
+from app.auth.auth_core.crud import get_user
+from app.auth.auth_core import schemas
+from app.core.database import SessionLocal
+
+
+reuseable_oauth = OAuth2PasswordBearer(tokenUrl="/login", scheme_name="JWT")
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def get_current_user(token: str = Depends(reuseable_oauth)) -> schemas.UserAuth:
+
+    try:
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
+        token_data = schemas.TokenPayload(**payload)
+        if datetime.fromtimestamp(token_data.exp) < datetime.now():
+            raise HTTPException(
+                status_code=401,
+                detail="Token expired",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=403,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    db = next(get_db())
+    user = get_user(db, token_data.sub)
+
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Could not find user",
+        )
+
+    return schemas.UserAuth(username=user.username, role=user.role)
