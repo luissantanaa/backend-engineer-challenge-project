3fcab29f791d5481556ed46c225356e598abde50
santana <luissantanaa>
2024-04-22 10:44:57 +0100

    adds methods to auth utils

--- app/auth/auth_utils/auth_utils.py
+++ app/auth/auth_utils/auth_utils.py
@@ -1,7 +1,51 @@
-import bcrypt
+import os
+
+
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from typing import Union, Any
+from jose import jwt
+
+
+REFRESH_TOKEN_EXPIRE_MINUTES = os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_REFRESH_SECRET_KEY = os.getenv("JWT_REFRESH_SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hashPassword(password: str) -> str:
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(bytes(password, encoding="utf-8"), salt)
-    return str(hashed_password)
+    return pwd_context.hash(password)
+
+
+def verifyPassword(password: str, hashed_password: str) -> str:
+    return pwd_context.verify(password, hashed_password)
+
+
+def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.now() + expires_delta
+    else:
+        expires_delta = datetime.now() + timedelta(
+            minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES)
+        )
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
+
+
+def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.now() + expires_delta
+    else:
+        expires_delta = datetime.now() + timedelta(
+            minutes=int(REFRESH_TOKEN_EXPIRE_MINUTES)
+        )
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
