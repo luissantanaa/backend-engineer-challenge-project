d4b4bf07001ebc62884edd10cc081b5bf2da7827
santana <luissantanaa>
2024-04-24 11:04:00 +0100

    updates init.sql

--- init.sql
+++ init.sql
@@ -5,8 +5,7 @@ CREATE TABLE "users" (
   "id" serial PRIMARY KEY,
   "username" varchar NOT NULL UNIQUE,
   "hashed_password" varchar NOT NULL,
-  "role" varchar NOT NULL DEFAULT 'user',
-  "created_at" timestamp
+  "role" varchar NOT NULL DEFAULT 'user'
 );
 
 CREATE TABLE "data_points" (
@@ -14,8 +13,27 @@ CREATE TABLE "data_points" (
   "time" timestamptz NOT NULL,
   "value" DOUBLE PRECISION NOT NULL,
   "valid" boolean NOT NULL,
-  "tags" varchar[],
-  "created_at" timestamp
+  "tags" varchar[]
 );
 
-INSERT INTO USERS(id, username, hashed_password, role) VALUES(0, 'admin', '$2b$12$a/pMatsPyWuHa5KdqatsFOLZgPYjPZU4If4//jeretXqZ.ujoYFJG','admin')
+INSERT INTO USERS(id, username, hashed_password, role) VALUES(0, 'admin', '$2b$12$a/pMatsPyWuHa5KdqatsFOLZgPYjPZU4If4//jeretXqZ.ujoYFJG','admin');
+
+CREATE DATABASE test_data_points_db;
+\connect test_data_points_db;
+
+CREATE TABLE "users" (
+  "id" serial PRIMARY KEY,
+  "username" varchar NOT NULL UNIQUE,
+  "hashed_password" varchar NOT NULL,
+  "role" varchar NOT NULL DEFAULT 'user'
+);
+
+CREATE TABLE "data_points" (
+  "id" serial PRIMARY KEY,
+  "time" timestamptz NOT NULL,
+  "value" DOUBLE PRECISION NOT NULL,
+  "valid" boolean NOT NULL,
+  "tags" varchar[]
+);
+
+INSERT INTO USERS(id, username, hashed_password, role) VALUES(0, 'admin', '$2b$12$a/pMatsPyWuHa5KdqatsFOLZgPYjPZU4If4//jeretXqZ.ujoYFJG','admin');
